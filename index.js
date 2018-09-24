4ab4dd39f64770aa2792bcbcdc44cf71e076bf0a
Max Ivanov (Wax) <33900684+max7thuzh@users.noreply.github.com>
2018-09-24 14:30:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,155 @@
+"use strict";
+
+// ====================================
+
+const laptops = [
+  {
+    size: 13,
+    color: 'white',
+    price: 28000,
+    release_date: 2015,
+    name: 'Macbook Air White 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'gray',
+    price: 32000,
+    release_date: 2016,
+    name: 'Macbook Air Gray 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'black',
+    price: 35000,
+    release_date: 2017,
+    name: 'Macbook Air Black 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'white',
+    price: 45000,
+    release_date: 2015,
+    name: 'Macbook Air White 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'gray',
+    price: 55000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'black',
+    price: 45000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'white',
+    price: 65000,
+    release_date: 2015,
+    name: 'Macbook Air White 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'gray',
+    price: 75000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'black',
+    price: 80000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+];
+
+const refs = {
+  form: document.querySelector(".js-form"),
+  resetButton: document.querySelector(".js-reset"),
+  source: document.querySelector("#root")
+};
+
+const filter = {};
+
+// ====================================
+
+refs.form.addEventListener("submit", handleFormSubmit);
+refs.resetButton.addEventListener("click", handleResetButtonClick);
+
+// ====================================
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const input = {
+    size: document.querySelector('input[name="size"]:checked'),
+    color: document.querySelector('input[name="color"]:checked'),
+    release_date: document.querySelector('input[name="release_date"]:checked')
+  };
+
+  filter.size = input.size.value;
+  filter.color = input.color.value;
+  filter.release_date = input.release_date.value;
+
+  getMarkup(filter);
+}
+
+function getMarkup(obj) {
+  const userChoiceObj = laptops.find(
+    element =>
+      element.size == obj.size &&
+      element.color === obj.color &&
+      element.release_date == obj.release_date
+  );
+
+  if (userChoiceObj === undefined) {
+    const messageMarkup = `<p class="message">Not found!</p>`;
+    refs.source.innerHTML = messageMarkup;
+
+    refs.form.reset();
+  } else {
+    const cardTemplate = document.querySelector("#card").innerHTML.trim();
+    const fnHandlebars = Handlebars.compile(cardTemplate);
+    const markup = fnHandlebars(userChoiceObj);
+
+    refs.source.innerHTML = markup;
+
+    refs.form.reset();
+  }
+}
+
+function handleResetButtonClick() {
+  refs.source.innerHTML = "";
+}
